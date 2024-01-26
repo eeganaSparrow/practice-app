f04926d19b2aec1cd843465f1be7c4637272933b
sparrow <66nozorome@gmail.com>
2024-01-26 23:24:59 +0900

    フォーム処理の追加

--- app/Http/Requests/Tweet/CreateRequest.php
+++ app/Http/Requests/Tweet/CreateRequest.php
@@ -11,7 +11,7 @@ class CreateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class CreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'tweet' => 'required|max:140',
         ];
+    }
+
+    public function tweet():string{
+        return $this->input('tweet');
     }
 }
--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -8,6 +8,19 @@
 <body>
     <h1>Sparrow Tweet</h1>
     <div>
+        <form action="{{ route('tweet.create') }}" method="post">
+            @csrf
+            <label for="tweet-content">つぶやき</label>
+            <span>140文字まで</span>
+            <textarea id="tweet-content" name="tweet" type="text" placeholder="つぶやき入力"></textarea>
+            @error('tweet')
+                <p style="color: red;">{{ $message }}</p>
+            @enderror
+            <button type="submit">投稿</button>
+        </form>
+    </div>
+
+    <div>
         @foreach($tweets as $tweet)
             <p>{{ $tweet->content }}</p>
         @endforeach

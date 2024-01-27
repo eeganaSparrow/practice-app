6686f0f2d78180456838161a414f2a162d88f187
sparrow <66nozorome@gmail.com>
2024-01-27 23:49:26 +0900

    削除機能追加

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -7,6 +7,9 @@
 </head>
 <body>
     <h1>Sparrow Tweet</h1>
+    @if(session('feedback.success'))
+        <p style="color: green;">{{ session('feedback.success')}}</p>
+    @endif
     <div>
         <form action="{{ route('tweet.create') }}" method="post">
             @csrf
@@ -26,6 +29,11 @@
                 <summary>{{ $tweet->content }}</summary>
                 <div>
                     <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id])}}">編集</a>
+                    <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id])}}" method="post">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit">削除</button>
+                    </form>
                 </div>
             </details>
         @endforeach

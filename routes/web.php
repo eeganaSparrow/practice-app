1bdf7ae5db3e53e3a7e639debff9938365949137
sparrow <66nozorome@gmail.com>
2024-01-31 16:00:15 +0900

    /の遷移先をtweet画面に変更

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', \App\Http\Controllers\Tweet\IndexController::class);
+
 
 Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
 ->name('tweet.index');

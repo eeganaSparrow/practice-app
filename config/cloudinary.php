9b8f759eaba7d991cb5a1ee30f7e6ba9dd7085d5
sparrow <66nozorome@gmail.com>
2024-01-31 15:28:06 +0900

    cloudinaryの設定ファイルを修正

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -2,6 +2,6 @@
 
 return [
     'cloud_name' => env('CLOUDINARY_CLOUD_NAME', null),
-    'api_key' => env('CLOUDINARY_KEY', null),
+    'api_key' => env('CLOUDINARY_API_KEY', null),
     'api_secret' => env('CLOUDINARY_API_SECRET', null),
 ];
